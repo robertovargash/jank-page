f1f39b39c8338d1fb944bccbdafc442c0437ea10
robertovargash <robertovargashdez@gmail.com>
2025-11-25 10:02:01 -0500

    ghpages 5

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/agencia-janka/',
+  base: '/jank-page/',
   server: {
     port: 3000,
     open: true
